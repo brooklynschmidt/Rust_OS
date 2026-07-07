02ef2f17923dbe48ec1c02af6ec0ba9d4363b05f
brooklynschmidt <schmidt.bro@northeastern.edu>
2026-07-07 15:21:06 -0400

    created bootimage, runs kernel in QEMU and prints to VGA text buffer + more setup

--- os/src/main.rs
+++ os/src/main.rs
@@ -5,13 +5,26 @@ Unwinding disabled as it uses OS-specific libraries
 No access to Rust runtime or crt0, must overwrite it
     - Runtime is what sets up the execution environment prior to main
 */
+
 #![no_std]
 #![no_main]
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8; // cast to raw pointer
+    
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            // write to buffer the byte + color
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
