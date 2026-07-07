3d777914c8f331ada6cb9e83ead5ebc31088716f
brooklynschmidt <schmidt.bro@northeastern.edu>
2026-07-07 13:38:38 -0400

    free standing rust binary

---
+++ os/src/main.rs
@@ -0,0 +1,22 @@
+/*
+no_std -> Can't use standard library as that code depends on the OS
+panic_handler -> Function that the compiler invokes on a panic
+Unwinding disabled as it uses OS-specific libraries
+No access to Rust runtime or crt0, must overwrite it
+    - Runtime is what sets up the execution environment prior to main
+*/
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
