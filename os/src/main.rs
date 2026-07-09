2f8ae33e2831f097afd4aaf09a073fc551404bac
brooklynschmidt <schmidt.bro@northeastern.edu>
2026-07-09 14:19:36 -0400

    created custom test framework for kernel, added tests for vga buffer, *_print macros and should_panic cases

--- os/src/main.rs
+++ os/src/main.rs
@@ -8,21 +8,35 @@ No access to Rust runtime or crt0, must overwrite it
 
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-mod vga_buffer;
+use os::println;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Testing panic");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    println!("{}", info);
+    println!("{}", _info);
     loop {}
 }
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    os::test_panic_handler(info)
+}
+
+
 
