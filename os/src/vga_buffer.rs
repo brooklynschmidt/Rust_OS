d05603d01024a081d012c0e29aa2276103a14357
brooklynschmidt <schmidt.bro@northeastern.edu>
2026-07-14 23:13:56 -0400

    fixed race condition in tests

--- os/src/vga_buffer.rs
+++ os/src/vga_buffer.rs
@@ -192,14 +192,25 @@ fn test_println_many() {
     }
 }
 
+/*
+Potential race condition where the timer interrupt handler might run between the println and the reading of the screen characters.
+
+Solution: Keep WRITER locked for complete duration of the test
+Interrupt handler can't write anything to the screen
+*/
 #[test_case]
 fn test_println_output() {
+    use x86_64::instructions::interrupts;
+    
     let s = "Some test string that fits on a single line";
-    println!("{}", s);
-    for (i, c) in s.chars().enumerate() {
-        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
-        assert_eq!(char::from(screen_char.ascii_character), c);
-    }
+    interrupts::without_interrupts(|| {
+        let mut writer = WRITER.lock();
+        writeln!(writer, "\n{}", s).expect("writeln failed");
+        for (i, c) in s.chars().enumerate() {
+            let screen_char = writer.buffer.chars[BUFFER_HEIGHT - 2][i].read();
+            assert_eq!(char::from(screen_char.ascii_character), c);
+        }
+    });
 }
 
 
