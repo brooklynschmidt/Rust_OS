0815e4434ea306623ed706844387aafcbeb34e5e
brooklynschmidt <schmidt.bro@northeastern.edu>
2026-07-16 14:28:00 -0400

    traverse level 3 tables

--- os/src/main.rs
+++ os/src/main.rs
@@ -32,6 +32,7 @@ physical_memory_offset tells us the virtual start address of the physical memory
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use os::memory::active_level_4_table;
     use x86_64::VirtAddr;
+    use x86_64::structures::paging::PageTable;
 
     println!("Hello World{}", "!");
     os::init();
@@ -42,6 +43,18 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     for (i, entry) in l4_table.iter().enumerate() {
         if !entry.is_unused() {
             println!("L4 Entry {}: {:?}", i, entry);
+
+            let phys = entry.frame().unwrap().start_address();
+            let virt = phys.as_u64() + boot_info.physical_memory_offset;
+            let ptr = VirtAddr::new(virt).as_mut_ptr();
+            let l3_table: &PageTable = unsafe { &*ptr };
+
+            // Print non-empty entries of the L3 table
+            for (i, entry) in l3_table.iter().enumerate() {
+                if !entry.is_unused() {
+                    println!(" L3 Entry {}: {:?}", i, entry);
+                }
+            }
         }
     }
 
